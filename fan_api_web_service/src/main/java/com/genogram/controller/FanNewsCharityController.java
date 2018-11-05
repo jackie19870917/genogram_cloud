d146c7a3d609ea32b6dc67fee83e9883f8de97e6
wang.wei <wei.wang40@dxc.com>
2018-11-05 14:12:54 +0800

    Default Changelist

---
+++ fan_api_web_service/src/main/java/com/genogram/controller/FanNewsCharityController.java
@@ -0,0 +1,20 @@
+package com.genogram.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ * 联谊会-家族慈善财 前端控制器
+ * </p>
+ *
+ * @author wangwei
+ * @since 2018-11-05
+ */
+@Controller
+@RequestMapping("/genogram/fanNewsCharity")
+public class FanNewsCharityController {
+
+}
+
---
+++ fan_api_web_service/src/main/java/com/genogram/controller/FanNewsIndustryController.java
@@ -0,0 +1,21 @@
+package com.genogram.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 联谊会-家族产业 前端控制器
+ * </p>
+ *
+ * @author wangwei
+ * @since 2018-11-05
+ */
+@Controller
+@RequestMapping("/genogram/fanNewsIndustry")
+public class FanNewsIndustryController {
+
+}
+
