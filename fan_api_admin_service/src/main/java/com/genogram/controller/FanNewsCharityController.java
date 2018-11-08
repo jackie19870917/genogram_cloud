b313eb3ee15812a41d31d899447b9f4cddc8ecf7
xiaocong <554221180@qq.com>
2018-11-08 10:55:43 +0800

    Default Changelist

--- fan_api_admin_service/src/main/java/com/genogram/controller/FanNewsCharityController.java
+++ fan_api_admin_service/src/main/java/com/genogram/controller/FanNewsCharityController.java
@@ -1,4 +1,21 @@
 package com.genogram.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 联谊会-家族慈善财 后台控制器
+ * </p>
+ *
+ * @author wangwei
+ * @since 2018-11-05
+ */
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("genogram/backstage/fanNewsCharity/")
 public class FanNewsCharityController {
+
+
 }
