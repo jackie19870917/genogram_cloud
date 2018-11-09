ca5063a1ab3f947d7379f039abbd02fbaf4ccd9a
xiaocong <554221180@qq.com>
2018-11-09 19:23:59 +0800

    Default Changelist

--- fan-biz-service/src/main/java/com/genogram/service/IFanNewsCharityService.java
+++ fan-biz-service/src/main/java/com/genogram/service/IFanNewsCharityService.java
@@ -1,5 +1,14 @@
 package com.genogram.service;
 
+/**
+ *
+ *@Author: Toxicant
+ *@Date: 2018-11-09
+ *@Time: 18:55
+ *@Param: 
+ *@return:
+ *@Description:
+*/
 public interface IFanNewsCharityService {
 
 }
