fc0c7be5c5533a2db7918687dcb64dd8ad1c75ac
wang.wei <wei.wang40@dxc.com>
2018-11-13 16:20:22 +0800

    add

---
+++ pro-biz-service/src/main/java/com/genogram/ProBizServiceApplication.java
@@ -0,0 +1,12 @@
+package com.genogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProBizServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProBizServiceApplication.class, args);
+	}
+}
---
+++ pro_api_web_service/src/main/java/com/genogram/ProApiWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.genogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProApiWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProApiWebServiceApplication.class, args);
+	}
+}
