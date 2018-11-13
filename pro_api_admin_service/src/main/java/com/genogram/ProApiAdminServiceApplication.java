7b473882c11c86aec868c2393c9b17017d49cbd0
wang.wei <wei.wang40@dxc.com>
2018-11-13 16:28:20 +0800

    add

---
+++ pro_api_admin_service/src/main/java/com/genogram/ProApiAdminServiceApplication.java
@@ -0,0 +1,12 @@
+package com.genogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProApiAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProApiAdminServiceApplication.class, args);
+	}
+}
