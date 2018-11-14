077b4f01704af160829108ac255352fcbfe13106
Wang.Wei.home <eric218@163.com>
2018-11-14 22:30:08 +0800

    add model

---
+++ common_payment/src/main/java/com/genogram/CommonPaymentApplication.java
@@ -0,0 +1,12 @@
+package com.genogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonPaymentApplication.class, args);
+    }
+}
