1d89fd7feb9c1ed0842f3de369a53e97d03e7536
wang.wei <wei.wang40@dxc.com>
2018-11-20 14:15:32 +0800

    Default Changelist

--- fan_api_admin_service/src/main/java/com/genogram/config/CommonConfig.java
+++ fan_api_admin_service/src/main/java/com/genogram/config/CommonConfig.java
@@ -1,18 +1,13 @@
 package com.genogram.config;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.servlet.MultipartConfigElement;
 
 /**
- * SwaggerConfig
+ * commonConfig
  *
  * @author Toxicant
  * @date 2016/10/31
@@ -20,13 +15,13 @@ import javax.servlet.MultipartConfigElement;
 @Configuration
 public class CommonConfig {
     @Bean
-public MultipartConfigElement multipartConfigElement() {
-    MultipartConfigFactory factory = new MultipartConfigFactory();
-    //文件最大
-    factory.setMaxFileSize("10MB"); //KB,MB
-    /// 设置总上传数据总大小
-    factory.setMaxRequestSize("20MB");
-    return factory.createMultipartConfig();
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大50MB
+        factory.setMaxFileSize("50MB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("50MB");
+        return factory.createMultipartConfig();
     }
 
 }
