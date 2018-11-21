7b7b9b23e7f733e0b5d1c5d8ae34a8da223da757
xiaocong <554221180@qq.com>
2018-11-21 11:27:27 +0800

    Default Changelist

---
+++ common_service/src/main/java/com/genogram/entityvo/UserVo.java
@@ -0,0 +1,76 @@
+package com.genogram.entityvo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UserVo {
+
+    private Integer id;
+    /**
+     * 用户主键，user开头，日期年月日小时分秒+随机数组成
+     */
+    private String userId;
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 真实姓名
+     */
+    private String realName;
+    /**
+     * 别名
+     */
+    private String nickName;
+    /**
+     * 手机号
+     */
+    private String mobilePhone;
+    /**
+     * 头像url
+     */
+    private String picSrc;
+    /**
+     * 密码
+     *//*
+    private String password;*/
+    /**
+     * 状态(0:删除;1:正常;2:异常)
+     */
+    private Integer status;
+    /**
+     * 网站id(省或者县的网站id)
+     */
+    private Integer siteId;
+    /**
+     * 角色1.(县级管理员,省级管理员)
+     */
+    private Integer role;
+    /**
+     * sys_family:id
+     */
+    private Integer familyCode;
+    private Integer regionCode;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 创建人
+     */
+    private Integer createUser;
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+    /**
+     * 修改人
+     */
+    private Integer updateUser;
+    /**
+     * token
+     */
+    private String token;
+}
--- fan_api_web_service/src/main/java/com/genogram/controller/FanUserLoginController.java
+++ fan_api_web_service/src/main/java/com/genogram/controller/FanUserLoginController.java
@@ -1,6 +1,7 @@
 package com.genogram.controller;
 
 import com.genogram.entity.AllUserLogin;
+import com.genogram.entityvo.UserVo;
 import com.genogram.service.IAllUserLoginService;
 import com.genogram.unit.DateUtil;
 import com.genogram.unit.Response;
@@ -9,6 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
@@ -43,10 +45,10 @@ public class FanUserLoginController {
      // @param allUserLogin
      * @return
      */
-    @ApiOperation("登陆")
+    @ApiOperation(value = "登陆",notes = "userName:用户名,realName:真实名,nickName:别名,mobilePhone:手机,picUrl:头像,siteId:网站Id,role:角色(1-县级管理员,2-省级管理员,0-不是管理员),familyCode:姓氏,region:地区,token:token")
     @RequestMapping(value = "login",method = RequestMethod.POST)
-    public Response<AllUserLogin> getAllUserLogin(@ApiParam("用户名")@RequestParam String userName,
-                                                  @ApiParam("密码")@RequestParam String password) {
+    public Response<UserVo> getAllUserLogin(@ApiParam("用户名")@RequestParam String userName,
+                                            @ApiParam("密码")@RequestParam String password) {
 
         AllUserLogin allUserLogin = new AllUserLogin();
         allUserLogin.setMobilePhone(userName);
@@ -67,18 +69,15 @@ public class FanUserLoginController {
                 String value = userLogin.getId()+"="+userLogin.getUserName();
                 map.put(user, value);
 
-//                String token = DigestUtils.md5DigestAsHex(map.toString().getBytes());
-//                System.out.println(token);
-
                 //Base64加密
                 byte[] bytes = Base64.encodeBase64(map.toString().getBytes(),true);
                 String str = new String(bytes);
 
-              /*  List<String> strings = Arrays.asList(str.split("="));
-                for (String string : strings) {
-                    System.out.println(string);
-                }*/
-                return ResponseUtlis.success(str);
+                UserVo userVo = new UserVo();
+                BeanUtils.copyProperties(userLogin,userVo);
+                userVo.setToken(str);
+
+                return ResponseUtlis.success(userVo);
             } else {
                 return ResponseUtlis.error(500, "用户名或密码错误");
             }
@@ -91,7 +90,7 @@ public class FanUserLoginController {
      * @param allUserLogin
      * @return
      */
-    @ApiOperation("注册")
+    @ApiOperation(value = "注册",notes = "userName:用户名,mobilePhone:手机号,password:密码,familyCode:姓氏,region:地区")
     @RequestMapping(value = "signIn", method = RequestMethod.POST)
     public Response<AllUserLogin> insertAllUserLogin(AllUserLogin allUserLogin) {
 
