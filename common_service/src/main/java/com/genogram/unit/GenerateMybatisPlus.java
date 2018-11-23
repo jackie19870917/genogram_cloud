a6116d9ea986a90b46cf721c87989cbc2b0ca60f
wang.wei <wei.wang40@dxc.com>
2018-11-23 10:48:54 +0800

    Default Changelist

---
+++ common_service/src/main/java/com/genogram/unit/GenerateMybatisPlus.java
@@ -0,0 +1,75 @@
+package com.genogram.unit;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+public class GenerateMybatisPlus {
+    final static String  dirPath = "C:\\genogram_cloud\\";
+    public void contextLoads() {
+        AutoGenerator mpg = new AutoGenerator();
+
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(dirPath);
+        gc.setAuthor("wangwei");//作者
+        gc.setFileOverride(true); //是否覆盖
+        gc.setActiveRecord(true);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(true);// XML columList
+
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setTypeConvert(new MySqlTypeConvert(){
+            @Override
+            public DbColumnType processTypeConvert(String fieldType) {
+                return super.processTypeConvert(fieldType);
+            }
+        });
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("875Yhtpw758@");
+        dsc.setUrl("jdbc:mysql://192.168.2.179:3306/genealogy?serverTimezone=UTC");
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setInclude(new String[] { "all_message_board"}); // 需要生成的表
+        //strategy.setExclude(new String[]{"all_family"}); // 排除生成的表
+        strategy.setEntityBuilderModel(true);
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com");
+        pc.setModuleName("genogram");
+        pc.setController("controler");
+        pc.setEntity("entity");
+        pc.setMapper("mapper");
+        pc.setService("service");
+        pc.setServiceImpl("serviceImpl");
+        pc.setXml("mapperXml");
+
+        mpg.setPackageInfo(pc);
+
+        mpg.execute();
+    }
+
+    public static void main(String[] args) {
+        GenerateMybatisPlus p = new GenerateMybatisPlus();
+        p.contextLoads();
+    }
+
+}
