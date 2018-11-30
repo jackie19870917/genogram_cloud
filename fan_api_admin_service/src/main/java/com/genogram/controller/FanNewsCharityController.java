738e5514bf1b05ce987adca2f63bd1829f287d1f
xiaocong <554221180@qq.com>
2018-11-30 10:11:23 +0800

    Default Changelist

--- fan_api_admin_service/src/main/java/com/genogram/controller/FanNewsCharityController.java
+++ fan_api_admin_service/src/main/java/com/genogram/controller/FanNewsCharityController.java
@@ -214,8 +214,8 @@ public class FanNewsCharityController {
     @ApiOperation(value = "慈善收支(文章)草稿", notes = "id:主键,showId:显示位置,amount:支出金额,useFor:支出用途,newsTitle:标题,newsText:内容,visitNum:查看数")
     @RequestMapping(value = "insertOrUpdateFanNewsCharityOutDeft", method = RequestMethod.POST)
     public Response<NewsCharityOutVo> insertOrUpdateFanNewsCharityOutDeft(FanNewsCharityOut fanNewsCharityOut,
-                                                                          @ApiParam("图片名称")@RequestParam(value = "fileName",required = false) String fileName,
-                                                                          @ApiParam("图片url") @RequestParam(value = "filePath",required = false)String filePath,
+                                                                          @ApiParam("图片名称") String fileName,
+                                                                          @ApiParam("图片url") String filePath,
                                                                           @ApiParam("token") @RequestParam(value = "token", required = false) String token) {
 
         if (StringUtils.isEmpty(token)) {
