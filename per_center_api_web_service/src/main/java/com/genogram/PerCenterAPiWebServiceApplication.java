6d1f0624fd5ced53ecbe774af471de788c69dad0
yizx123456 <1790615925@qq.com>
2018-12-05 16:40:50 +0800

    个人中心web-api-service

---
+++ per_center_api_web_service/src/main/java/com/genogram/PerCenterAPiWebServiceApplication.java
@@ -0,0 +1,22 @@
+package com.genogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @Author: yizx
+ * @Date: 2018--12--05
+ * @Time: 10:59
+ * @return:
+ * @Description:
+ *
+ */
+@SpringBootApplication
+public class PerCenterAPiWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerCenterAPiWebServiceApplication.class, args);
+    }
+
+}
