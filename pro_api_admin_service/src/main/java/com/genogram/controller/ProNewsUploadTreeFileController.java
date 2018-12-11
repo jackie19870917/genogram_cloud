c61e08c820d42d3843e3e95e8690cc25b67c215c
xiaocong <554221180@qq.com>
2018-12-11 14:03:05 +0800

    Default Changelist

--- pro_api_admin_service/src/main/java/com/genogram/controller/ProNewsUploadTreeFileController.java
+++ pro_api_admin_service/src/main/java/com/genogram/controller/ProNewsUploadTreeFileController.java
@@ -99,6 +99,7 @@ public class ProNewsUploadTreeFileController {
     @ApiOperation(value = "电子谱详情", notes = "id-主键,regionCode-地区,isFrom-来源(1-省级,2-县级),filePath-文件路径,fileName-文件名称,contactUser-联系人,status-状态(1-公开,2-密码访问,3-私密,0-删除),password-密码,preThirty-前三十页(1-显示,2-不显示)")
     @RequestMapping(value = "getProNewsUploadTreeFile", method = RequestMethod.POST)
     public Response<ProNewsUploadTreeFile> getProNewsUploadTreeFile(@ApiParam("主键") @RequestParam("id") Integer id,
+                                                                    @ApiParam("密码") @RequestParam(value = "password", required = false) String password,
                                                                     @ApiParam("来源") @RequestParam("isFrom") Integer isFrom,
                                                                     @ApiParam("token") @RequestParam(value = "token", required = false) String token) {
 
@@ -114,11 +115,25 @@ public class ProNewsUploadTreeFileController {
 
         if (isFrom == 1) {
             ProNewsUploadTreeFile proNewsUploadTreeFile = proNewsUploadTreeFileService.getProNewsUploadTreeFile(id);
+
+            if (proNewsUploadTreeFile.getStatus() == 2) {
+                if (!proNewsUploadTreeFile.getPassword().equals(password)) {
+                    return ResponseUtlis.error(Constants.ERRO_CODE, "密码错误");
+                }
+            }
+
             return ResponseUtlis.success(proNewsUploadTreeFile);
 
         } else {
 
             FanNewsUploadTreeFile fanNewsUploadTreeFile = proNewsUploadTreeFileService.getFanNewsUploadTreeFile(id);
+
+            if (fanNewsUploadTreeFile.getStatus() == 2) {
+                if (!fanNewsUploadTreeFile.getPassword().equals(password)) {
+                    return ResponseUtlis.error(Constants.ERRO_CODE, "密码错误");
+                }
+            }
+
             return ResponseUtlis.success(fanNewsUploadTreeFile);
         }
 
