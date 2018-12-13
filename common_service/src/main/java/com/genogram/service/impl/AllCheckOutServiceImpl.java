742a5da80b71296cc13d88ecaf10e79e4edd99ee
xiaocong <554221180@qq.com>
2018-12-13 17:57:37 +0800

    Default Changelist

--- common_service/src/main/java/com/genogram/service/impl/AllCheckOutServiceImpl.java
+++ common_service/src/main/java/com/genogram/service/impl/AllCheckOutServiceImpl.java
@@ -4,6 +4,7 @@ import com.genogram.service.IAllCheckOutService;
 import com.genogram.unit.SensitivewordFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.Set;
 
@@ -15,6 +16,10 @@ public class AllCheckOutServiceImpl implements IAllCheckOutService {
 
     @Override
     public Set getSensitiveWord(String string) {
+
+        if (StringUtils.isEmpty(string)) {
+            return null;
+        }
 
         SensitivewordFilter sensitivewordFilter = new SensitivewordFilter();
 
