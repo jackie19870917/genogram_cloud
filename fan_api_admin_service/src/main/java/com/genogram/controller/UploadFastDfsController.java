54e5ba1d019a95d7cefc9ee6a48ca2c096709c0d
yuzhou <980821763@qq.com>
2019-01-07 17:54:30 +0800

    Default Changelist

--- fan_api_admin_service/src/main/java/com/genogram/controller/UploadFastDfsController.java
+++ fan_api_admin_service/src/main/java/com/genogram/controller/UploadFastDfsController.java
@@ -1,5 +1,6 @@
 package com.genogram.controller;
 
+import com.genogram.config.Constants;
 import com.genogram.service.IUploadFastDfsService;
 import com.genogram.service.IUploadFileService;
 import com.genogram.unit.Response;
@@ -7,9 +8,13 @@ import com.genogram.unit.ResponseUtlis;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -28,6 +33,8 @@ public class UploadFastDfsController {
     private IUploadFastDfsService uploadFastDfsService;
     @Autowired
     private IUploadFileService uploadFileService;
+    @Autowired
+    private ConventerController conventerController;
 
     /**
      * 文件上传
@@ -39,9 +46,25 @@ public class UploadFastDfsController {
      * @return:
      * @Description:
      */
-    @ApiOperation(value = "上传文件", notes = "返回文件src")
+    /*@ApiOperation(value = "上传文件", notes = "返回文件src")
     @RequestMapping(value = "/uploadFastdfs", method = RequestMethod.POST)
     public Response<Map> uploadFastdfs(MultipartFile file) {
         return ResponseUtlis.success(uploadFastDfsService.uploadFastDfs(file));
+    }*/
+
+    @ApiOperation(value = "上传文件", notes = "返回文件src")
+    @RequestMapping(value = "/uploadFastdfs", method = RequestMethod.POST)
+    public Response<Map> uploadFastdfs(MultipartFile file, @RequestParam(value = "isGenealogy",defaultValue = "0") Integer isGenealogy,
+                                       HttpServletResponse response, HttpServletRequest request, Model model) throws IOException {
+        Map<String, Object> stringObjectMap = uploadFastDfsService.uploadFastDfs(file);
+        if(isGenealogy!=0){
+            Object file_path = stringObjectMap.get("file_path");
+            String filePath = Constants.ALIYUN_IP + file_path;
+            //电子谱文件名称
+            String treePreviewPath = conventerController.fileConventer(filePath, model, request, response);
+            filePath=filePath+"@"+Constants.ELECTRONIC_SPECTRUM_PREVIEW_IP + treePreviewPath;
+            stringObjectMap.put("file_path",filePath);
+        }
+        return ResponseUtlis.success(stringObjectMap);
     }
 }
