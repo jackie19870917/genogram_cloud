6a88d73ca2cec5272a8033e59e8bc1f5138d34bb
yuzhou <980821763@qq.com>
2019-01-11 14:15:10 +0800

    Default Changelist

--- fan_api_web_service/src/main/java/com/genogram/FanApiWebServiceApplication.java
+++ fan_api_web_service/src/main/java/com/genogram/FanApiWebServiceApplication.java
@@ -2,19 +2,25 @@ package com.genogram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
- * @Author: wang, wei
+ * @Author: yizx
  * @Date: 2018-11-05
  * @Time: 22:23
  * @return:
  * @Description:
  */
 @SpringBootApplication
-public class FanApiWebServiceApplication {
+public class FanApiWebServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FanApiWebServiceApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FanApiWebServiceApplication.class);
     }
 
 }
