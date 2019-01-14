37712892d6a8cf57ccc5b70d82fd5ddb925afbd5
yuzhou <980821763@qq.com>
2019-01-14 16:25:38 +0800

    Default Changelist

--- common_service/src/main/java/com/genogram/entity/ProNewsUploadTreeFile.java
+++ common_service/src/main/java/com/genogram/entity/ProNewsUploadTreeFile.java
@@ -1,23 +1,20 @@
 package com.genogram.entity;
 
 import com.baomidou.mybatisplus.enums.IdType;
-
 import java.util.Date;
-
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.activerecord.Model;
 import com.baomidou.mybatisplus.annotations.TableName;
-
 import java.io.Serializable;
 
 /**
  * <p>
- *
+ * 
  * </p>
  *
  * @author wangwei
- * @since 2018-12-14
+ * @since 2019-01-14
  */
 @TableName("pro_news_upload_tree_file")
 public class ProNewsUploadTreeFile extends Model<ProNewsUploadTreeFile> {
@@ -239,21 +236,21 @@ public class ProNewsUploadTreeFile extends Model<ProNewsUploadTreeFile> {
     @Override
     public String toString() {
         return "ProNewsUploadTreeFile{" +
-                ", id=" + id +
-                ", siteId=" + siteId +
-                ", regionCode=" + regionCode +
-                ", isFrom=" + isFrom +
-                ", familyCode=" + familyCode +
-                ", filePath=" + filePath +
-                ", fileName=" + fileName +
-                ", contactUser=" + contactUser +
-                ", status=" + status +
-                ", password=" + password +
-                ", preThirty=" + preThirty +
-                ", createTime=" + createTime +
-                ", createUser=" + createUser +
-                ", updateTime=" + updateTime +
-                ", updateUser=" + updateUser +
-                "}";
+        ", id=" + id +
+        ", siteId=" + siteId +
+        ", regionCode=" + regionCode +
+        ", isFrom=" + isFrom +
+        ", familyCode=" + familyCode +
+        ", filePath=" + filePath +
+        ", fileName=" + fileName +
+        ", contactUser=" + contactUser +
+        ", status=" + status +
+        ", password=" + password +
+        ", preThirty=" + preThirty +
+        ", createTime=" + createTime +
+        ", createUser=" + createUser +
+        ", updateTime=" + updateTime +
+        ", updateUser=" + updateUser +
+        "}";
     }
 }
