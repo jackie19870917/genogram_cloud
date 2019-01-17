38cafc20ffa3039e726cf5085f86e490765bcc67
yizx123456 <1790615925@qq.com>
2019-01-17 16:12:25 +0800

    Default Changelist

---
+++ common_service/src/main/java/com/genogram/mapper/PuBaseInfoMapper.java
@@ -0,0 +1,16 @@
+package com.genogram.mapper;
+
+import com.genogram.entity.PuBaseInfo;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yizx
+ * @since 2019-01-16
+ */
+public interface PuBaseInfoMapper extends BaseMapper<PuBaseInfo> {
+
+}
