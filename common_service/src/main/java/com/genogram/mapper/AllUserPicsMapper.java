ba9f9ad8cadfcf6a66211cfa699569a3a455ffb5
yizx123456 <1790615925@qq.com>
2019-01-17 16:13:21 +0800

    Default Changelist

--- common_service/src/main/java/com/genogram/mapper/AllUserPicsMapper.java
+++ common_service/src/main/java/com/genogram/mapper/AllUserPicsMapper.java
@@ -1,7 +1,9 @@
 package com.genogram.mapper;
 
+import com.baomidou.mybatisplus.service.IService;
 import com.genogram.entity.AllUserPics;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.genogram.entity.PuBaseInfo;
 
 /**
  * <p>
@@ -13,4 +15,15 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  */
 public interface AllUserPicsMapper extends BaseMapper<AllUserPics> {
 
+    /**
+     * <p>
+     *  服务类
+     * </p>
+     *
+     * @author yizx
+     * @since 2019-01-16
+     */
+    interface IPuBaseInfoService extends IService<PuBaseInfo> {
+
+    }
 }
