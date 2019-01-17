bf16c8f2d095098b493eebd22160d1465cb4493c
yizx123456 <1790615925@qq.com>
2019-01-17 16:12:10 +0800

    Default Changelist

---
+++ common_service/src/main/java/com/genogram/service/IPuBaseInfoService.java
@@ -0,0 +1,16 @@
+package com.genogram.service;
+
+import com.genogram.entity.PuBaseInfo;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yizx
+ * @since 2019-01-16
+ */
+public interface IPuBaseInfoService extends IService<PuBaseInfo> {
+
+}
