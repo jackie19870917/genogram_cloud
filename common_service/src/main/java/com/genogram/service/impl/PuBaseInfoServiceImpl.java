f788762fcd57ef9d1da0699fe600e00e94dd2fc3
yizx123456 <1790615925@qq.com>
2019-01-17 16:12:45 +0800

    Default Changelist

---
+++ common_service/src/main/java/com/genogram/service/impl/PuBaseInfoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.genogram.service.impl;
+
+import com.genogram.entity.PuBaseInfo;
+import com.genogram.mapper.AllUserPicsMapper;
+import com.genogram.mapper.PuBaseInfoMapper;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author yizx
+ * @since 2019-01-16
+ */
+@Service
+public class PuBaseInfoServiceImpl extends ServiceImpl<PuBaseInfoMapper, PuBaseInfo> implements AllUserPicsMapper.IPuBaseInfoService {
+
+}
