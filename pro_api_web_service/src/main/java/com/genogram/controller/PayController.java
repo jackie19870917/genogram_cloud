e53bb515f620468a1abe68c520b725062b88a80d
Toxicant <554221180@qq.com>
2019-01-30 14:18:20 +0800

    Default Changelist

--- pro_api_web_service/src/main/java/com/genogram/controller/PayController.java
+++ pro_api_web_service/src/main/java/com/genogram/controller/PayController.java
@@ -351,7 +351,7 @@ public class PayController {
         proNewsCharityPayInService.insertProNewsCharityPayIn(proNewsCharityPayIn);
 
         // 回调地址
-        String callback ="http://www.yhtpw.com/8070/genogram/pay/callBack";
+        String callback ="47.105.177.1:8070/genogram/pay/callBack";
 
         // 生成一个code_url
         String codeUrl = PayUtils.pay(payId, userIp, totalFee, body, callback);
